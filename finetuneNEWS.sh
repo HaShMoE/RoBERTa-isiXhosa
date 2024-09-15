f3fd45e0f18c2ccc0321db4b9fe1e257c2f7a07b
HaShMoE <125799448+HaShMoE@users.noreply.github.com>
2024-09-15 11:09:02 +0200

    Update finetuneNEWS.sh

--- finetuneNEWS.sh
+++ finetuneNEWS.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-#PBS -N finetune_model
-#PBS -q gpu_1
-#PBS -l select=1:ncpus=7:ngpus=1
-#PBS -l walltime=8:00:00
-#PBS -P CSCI1674
-#PBS -m abe
-#PBS -WMail_Users=mhmhis005@uct.ac.za
-ulimit -s unlimited
-
-module load chpc/python/anaconda/3-2021.11
-module load chpc/cuda/11.6/SXM2/11.6
-module load chpc/openmpi/4.1.1/gcc-6.1.0
-source /home/hmahomed/myenv/bin/activate
-
-cd /home/hmahomed/lustre/roberta || exit
-
 now=$(date +%Y%m%d-%H:%M:%S:%3N)
 
 start=`date +%s`
@@ -24,8 +8,8 @@ LANG=xho
 for j in 1 2 3 4 5
 do
         export MAX_LENGTH=256
-        export ROBERTA_MODEL=/home/hmahomed/lustre/roberta/output/
-        export OUTPUT_DIR=/home/hmahomed/lustre/roberta/finetuned/NEWS/
+        export ROBERTA_MODEL=/RoBERTa-isiXhosa/output/
+        export OUTPUT_DIR=/RoBERTa-isiXhosa/finetuned/NEWS/
         export BATCH_SIZE=16
         export NUM_EPOCHS=20
         export SAVE_STEPS=10000
@@ -35,8 +19,8 @@ do
 
         # Run Python Script
         echo "Starting$j"
-        python3 /home/hmahomed/lustre/masakhane-news/code/train_textclass.py \
-                --data_dir /home/hmahomed/lustre/masakhane-news/data/${LANG}/ \
+        python3 /RoBERTa-isiXhosa/masakhane-news/code/train_textclass.py \
+                --data_dir /RoBERTa-isiXhosa/masakhane-news/data/${LANG}/ \
                 --model_type roberta \
                 --model_name_or_path $ROBERTA_MODEL \
                 --output_result $TEST_RESULT \
@@ -52,7 +36,7 @@ do
                 --save_steps $SAVE_STEPS \
                 --output_dir $OUTPUT_DIR \
                 --seed $SEED \
-                --overwrite_output_dir &>> "/home/hmahomed/lustre/roberta/finetuned/log_finetune_NEWS"
+                --overwrite_output_dir &>> "/RoBERTa-isiXhosa/finetuned/log_finetune_NEWS"
 done
 
 end=`date +%s`
