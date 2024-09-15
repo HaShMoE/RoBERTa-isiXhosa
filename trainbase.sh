85e26d8c97ce9136eac7035ff7f0957e5d32f575
HaShMoE <125799448+HaShMoE@users.noreply.github.com>
2024-09-15 11:06:11 +0200

    Update trainbase.sh

--- trainbase.sh
+++ trainbase.sh
@@ -12,15 +12,15 @@ python3 train_tokenizer.py
 echo "Training"
 python3 RoBERTa-isiXhosa/code/run_mlm.py \
         --model_type roberta \
-        --tokenizer_name ./model \
-        --config_name ./model \
-        --train_file ./wura-xh/train.txt \
-        --validation_file ./wura-xh/valid.txt \
+        --tokenizer_name RoBERTa-isiXhosa/model \
+        --config_name RoBERTa-isiXhosa/model \
+        --train_file RoBERTa-isiXhosa/dataset/wura-xh/train.txt \
+        --validation_file RoBERTa-isiXhosa/dataset/wura-xh/valid.txt \
         --do_train \
         --do_eval \
         --num_train_epochs 20 \
         --save_steps 1000 \
-        --output_dir ./output
+        --output_dir .RoBERTa-isiXhosa/output
 
 end=`date +%s`
 runtime=$(((end-start)/60))
